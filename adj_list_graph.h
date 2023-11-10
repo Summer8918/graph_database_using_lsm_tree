d3b6b005acd821be4c1e9bf95de7654e8f0bfafa
Milena Belianovich <112034622+milenabel@users.noreply.github.com>
2023-11-09 20:45:51 -0700

    commenting out the main()

--- adj_list_graph.h
+++ adj_list_graph.h
@@ -61,24 +61,24 @@ void display_AdjList(adjNode* ptr, int i)
     cout << endl;
 }
 // graph implementation
-int main()
-{
-    // graph edges array.
-    graphEdge edges[] = {
-        // (x, y, w) -> edge from x to y with weight w
-        {0,1,2},{0,2,4},{1,4,3},{2,3,2},{3,1,4},{4,3,3}
-    };
-    int N = 6;      // Number of vertices in the graph
-    // calculate number of edges
-    int n = sizeof(edges)/sizeof(edges[0]);
-    // construct graph
-    DiaGraph diagraph(edges, n, N);
-    // print adjacency list representation of graph
-    cout<<"Graph adjacency list "<<endl<<"(start_vertex, end_vertex, weight):"<<endl;
-    for (int i = 0; i < N; i++)
-    {
-        // display adjacent vertices of vertex i
-        display_AdjList(diagraph.head[i], i);
-    }
-    return 0;
-}
+// int main()
+// {
+//     // graph edges array.
+//     graphEdge edges[] = {
+//         // (x, y, w) -> edge from x to y with weight w
+//         {0,1,2},{0,2,4},{1,4,3},{2,3,2},{3,1,4},{4,3,3}
+//     };
+//     int N = 6;      // Number of vertices in the graph
+//     // calculate number of edges
+//     int n = sizeof(edges)/sizeof(edges[0]);
+//     // construct graph
+//     DiaGraph diagraph(edges, n, N);
+//     // print adjacency list representation of graph
+//     cout<<"Graph adjacency list "<<endl<<"(start_vertex, end_vertex, weight):"<<endl;
+//     for (int i = 0; i < N; i++)
+//     {
+//         // display adjacent vertices of vertex i
+//         display_AdjList(diagraph.head[i], i);
+//     }
+//     return 0;
+// }
